9d913a811da3c25f7e6b3035bc0a8bd67d429b1b
ravelsoares <ravelsoares64@gmail.com>
2022-05-21 20:18:00 -0300

    Adicionando gif

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -20,26 +20,42 @@ class _ResultPageState extends State<ResultPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height * 0.8,
-            child: ListView.builder(
-              itemCount: Provider.of<EvaluationList>(context).results.length,
-              itemBuilder: (context, index) {
-                return ResultItem(
-                    Provider.of<EvaluationList>(context).results[index]);
-              },
+      body: Provider.of<EvaluationList>(context).results.isNotEmpty
+          ? Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.8,
+                  child: ListView.builder(
+                    itemCount:
+                        Provider.of<EvaluationList>(context).results.length,
+                    itemBuilder: (context, index) {
+                      return ResultItem(
+                          Provider.of<EvaluationList>(context).results[index]);
+                    },
+                  ),
+                ),
+                const Center(
+                  child: Text(
+                    'Desenvolvido por Ravel Soares',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            )
+          : Column(
+              children: [
+                Center(
+                  child: Image.asset(
+                    'assets/gifs/without_evaluation.gif',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const Text(
+                  'Nenhuma avaliação feita',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                )
+              ],
             ),
-          ),
-          const Center(
-            child: Text(
-              'Desenvolvido por Ravel Soares',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          )
-        ],
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Provider.of<EvaluationList>(context, listen: false).loadEvaluations();
